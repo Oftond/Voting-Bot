224d0616153aa044219e756cffa3b3976b74d9d5
BigBoy2014 <superdr0chun14@gmail.com>
2025-04-17 16:25:41 +0700

    Add files via upload

---
+++ telegram bot/keyboard.py
@@ -0,0 +1,41 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+class keyboard:
+    @staticmethod
+    def get_start_keyboard():
+        return ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Удалить")],
+                [KeyboardButton(text="Создать голосование")],
+                [KeyboardButton(text="Проголосовать")],
+                [KeyboardButton(text="Статистика")],
+                [KeyboardButton(text="Справка")],
+            ],
+            resize_keyboard=True,
+            one_time_keyboard=True
+        )
+
+    @staticmethod
+    def get_cancel_keyboard():
+        return ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Отмена")],
+            ],
+            resize_keyboard=True,
+            one_time_keyboard=True
+        )
+
+    @staticmethod
+    def get_poll_options_keyboard(options: list):
+        """Создает клавиатуру с вариантами голосования и кнопкой отмены"""
+        buttons = [
+            [KeyboardButton(text=option)] for option in options
+        ]
+        buttons.append([KeyboardButton(text="Отмена")])
+
+        return ReplyKeyboardMarkup(
+            keyboard=buttons,
+            resize_keyboard=True,
+            one_time_keyboard=True
+        )
